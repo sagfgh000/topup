68a16085e71c97cff47bc80a09aa93ac451c5afc
Soul Land <rifattapader1@gmail.com>
2025-08-14 15:17:36 +0000

    what

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const securityHeaders = [
       value: 'max-age=63072000; includeSubDomains; preload'
     },
     {
-      key: 'X-Frame-Options',
-      value: 'DENY'
-    },
-    {
       key: 'X-Content-Type-Options',
       value: 'nosniff'
     }
